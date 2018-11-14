3a740a1c5937029b06edd5586e1cafe5d766ffd7
Mikhail Proniushkin <mikhailpro4@gmail.com>
2018-11-14 12:29:29 -0500

    layout setup

--- src/client/App.js
+++ src/client/App.js
@@ -16,8 +16,72 @@ class App extends Component {
 
 	render() {
 		return (
-			<div className="app">
-				app
+			<div className="app-container">
+
+				<div className="app-header">
+					<div className="dark-header">
+					</div>
+
+					<div className="light-header">
+					</div>
+				</div>
+
+				<div className="app-content">
+
+					<div className="left-sidebar">
+					 	<div className="sidbear-header">
+							<div className="sidebar-header-left">
+								Workbook content
+							</div>
+
+							<div className="sidebar-header-right">
+								x
+							</div>
+						</div>
+					</div>
+
+					<div className="main-area">
+
+						<div className="main-navigation-toolbar">
+							<div className="nav-header-left">
+								Workbook content | Global Code
+							</div>
+
+							<div className="nav-header-center">
+								Paths |  Graph
+							</div>
+
+							<div className="nav-header-right">
+								Console
+							</div>
+						</div>
+
+						<div className="main-area-sections-container">
+
+							<div className="main-area-section-global-code">
+								global code
+							</div>
+
+							<div className="main-area-section-general">
+								general area
+							</div>
+						</div>
+
+					</div>
+
+					<div className="right-sidebar">
+						<div className="sidbear-header">
+							<div className="sidebar-header-left">
+								Console
+							</div>
+
+							<div className="sidebar-header-right">
+								x
+							</div>
+						</div>
+					</div>
+				</div>
+
 			</div>
 		)
 	}
