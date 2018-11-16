00bb90f84f801fa489fca67f512c22944450a670
Mikhail Proniushkin <mikhailpro4@gmail.com>
2018-11-15 23:40:04 -0500

    shortcuts

--- src/client/App.js
+++ src/client/App.js
@@ -9,7 +9,14 @@ import SplitText from 'react-pose-text';
 import keydown from "react-keydown";
 import classNames from "classnames"
 import { Link } from "react-router-dom";
-import { Icon, Button, Intent } from "@blueprintjs/core";
+import {
+	Icon,
+	Button,
+	Intent,
+	Popover,
+  PopoverInteractionKind,
+  PopoverPosition
+} from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
 
 import {
@@ -28,7 +35,42 @@ FocusStyleManager.onlyShowFocusOnTabs();
 class App extends Component {
 	state = {
 		fullScreen: false,
-		transitionMinimimap: false
+		transitionMinimimap: false,
+		activePath: 0,
+		activeStep: 0,
+		activeNode: 0,
+		paths: [
+			{
+				id: 0,
+				length: 3,
+				name: "Path 1",
+				steps: 6
+			},
+			{
+				id: 1,
+				length: 4,
+				name: "Cleaning",
+				steps: 2
+			},
+			{
+				id: 2,
+				length: 3,
+				name: "Scenraio 1",
+				steps: 5
+			},
+			{
+				id: 3,
+				length: 3,
+				name: "Scneario 2",
+				steps: 3
+			},
+			{
+				id: 4,
+				length: 4,
+				name: "Analysis",
+				steps: 4
+			}
+		]
 	}
 
 	transitionMinimimap = () => {
@@ -45,6 +87,315 @@ class App extends Component {
 		}, 200)
 	}
 
+	@keydown("esc")
+	hideFullScreen() {
+		this.setState({
+			fullScreen: false
+		})
+	}
+
+	@keydown("shift+1")
+	toggleLeftSidebar() {
+		if(this.props.leftSidebarVisible) {
+			this.props.hideLeftSidebar()
+		} else {
+			this.props.showLeftSidebar()
+		}
+	}
+
+	@keydown("shift+2")
+	toggleGlobalCode() {
+		if(this.props.globalCodeVisible) {
+			this.props.hideGlobalCode()
+		} else {
+			this.props.showGlobalCode()
+		}
+	}
+
+	@keydown("shift+3")
+	toggleRightSidebar() {
+		if(this.props.rightSidebarVisible) {
+			this.props.hideRightSidebar()
+		} else {
+			this.props.showRightSidebar()
+		}
+	}
+
+	@keydown("shift+p")
+	openPaths() {
+		this.props.showPaths()
+		this.hideFullScreen()
+	}
+
+	@keydown("shift+g")
+	openGraph() {
+		this.props.showGraph()
+		this.hideFullScreen()
+	}
+
+	@keydown("shift+down")
+	selectNextPath() {
+		if(this.state.activePath == 4) {
+			this.setState({
+				activePath: 0
+			})
+		} else {
+			this.setState({
+				activePath: this.state.activePath + 1
+			})
+		}
+
+		this.setState({
+			activeStep: 0
+		})
+		console.log(this.state.activePath)
+	}
+
+	@keydown("shift+up")
+	selectPreviousPath() {
+		if(this.state.activePath == 0) {
+			this.setState({
+				activePath: 4
+			})
+		} else {
+			this.setState({
+				activePath: this.state.activePath - 1
+			})
+		}
+
+		this.setState({
+			activeStep: 0
+		})
+
+		console.log(this.state.activePath)
+	}
+
+	@keydown("down")
+	selectNextStep(event) {
+		if(this.state.activeStep == this.state.paths[this.state.activePath].steps-1) {
+			this.setState({
+				activeStep: 0
+			})
+		} else {
+			this.setState({
+				activeStep: this.state.activeStep + 1
+			})
+		}
+		console.log(this.state.activeStep)
+		event.preventDefault()
+		event.stopPropagation()
+	}
+
+	@keydown("up")
+	selectPreviousStep(event) {
+		if(this.state.activeStep == 0) {
+			this.setState({
+				activeStep: this.state.paths[this.state.activePath].steps -1
+			})
+		} else {
+			this.setState({
+				activeStep: this.state.activeStep - 1
+			})
+		}
+		console.log(this.state.activeStep)
+		event.preventDefault()
+		event.stopPropagation()
+	}
+
+	@keydown("right")
+	openFullScreen(event) {
+		event.preventDefault()
+		event.stopPropagation()
+		this.setState({
+			fullScreen: true
+		})
+	}
+
+	@keydown("left")
+	closeFullScreen(event) {
+		event.preventDefault()
+		event.stopPropagation()
+		this.setState({
+			fullScreen: false
+		})
+	}
+
+	renderPath = (path, i) => {
+		return (
+			<li
+				key={i}
+				className={
+					classNames({
+						"single-dummy-item-active": this.state.activePath == i
+					}, "single-dummy-item")
+				}
+			>
+				<div className="dummy-icon"/>
+				<div className={`dummy-bar dummy-bar-${path.length}`} />
+			</li>
+		)
+	}
+
+	renderPathSteps = (path) => {
+		let steps = []
+		for (var i = 0; i < path.steps; i++) {
+			steps.push(this.renderPathStep(path, i))
+		}
+		return (
+			<div className="steps-container">
+				{steps}
+			</div>
+		)
+	}
+
+	renderPathStep = (path, i) => {
+		return (
+			<div
+				key={i}
+				className="single-step-container"
+			>
+				<div
+					className="single-path-step"
+					className={
+						classNames({
+							"single-path-step-active": this.state.activeStep == i
+						}, "single-path-step")
+					}
+				>
+					<Button
+						minimal={true}
+						icon="arrow-right"
+						className="fullscreen"
+						onClick={() => { this.setState({
+								fullScreen: true
+							})}
+						}
+					/>
+				</div>
+
+				<div className="path-arrow"><Icon icon="arrow-down" /></div>
+			</div>
+		)
+	}
+
+	getPopoverContents = () => {
+		return (
+			<div className="help-container">
+				<div className="help-section">
+					<div className="help-section-title">
+						Switch main views
+					</div>
+
+					<ul className="help-action-list">
+						<li className="help-action">
+							<div className="help-action-description">
+								Toggle Paths
+							</div>
+
+							<div className="key">Shift + P</div>
+						</li>
+
+						<li className="help-action">
+							<div className="help-action-description">
+								Toggle Graph
+							</div>
+
+							<div className="key">Shift + G</div>
+						</li>
+					</ul>
+				</div>
+
+				<div className="help-section">
+					<div className="help-section-title">
+						Manage helpers
+					</div>
+
+					<ul className="help-action-list">
+						<li className="help-action">
+							<div className="help-action-description">
+								Toggle Workbook Contents
+							</div>
+
+							<div className="key">Shift + 1</div>
+						</li>
+
+						<li className="help-action">
+							<div className="help-action-description">
+								Toggle Global Code
+							</div>
+
+							<div className="key">Shift + 2</div>
+						</li>
+
+						<li className="help-action">
+							<div className="help-action-description">
+								Toggle Console
+							</div>
+
+							<div className="key">Shift + 3</div>
+						</li>
+					</ul>
+				</div>
+
+				<div className="help-section">
+					<div className="help-section-title">
+						Navigation
+					</div>
+
+					<ul className="help-action-list">
+						<li className="help-action">
+							<div className="help-action-description">
+								Select next Path
+							</div>
+
+							<div className="key">Shift + Down</div>
+						</li>
+
+						<li className="help-action">
+							<div className="help-action-description">
+								Select previous Path
+							</div>
+
+							<div className="key">Shift + Up</div>
+						</li>
+
+						<li className="help-action">
+							<div className="help-action-description">
+								Select next step
+							</div>
+
+							<div className="key">Down</div>
+						</li>
+
+						<li className="help-action">
+							<div className="help-action-description">
+								Select previous step
+							</div>
+
+							<div className="key">Up</div>
+						</li>
+
+						<li className="help-action">
+							<div className="help-action-description">
+								Open selected step in full screen editor
+							</div>
+
+							<div className="key">Right</div>
+						</li>
+
+						<li className="help-action">
+							<div className="help-action-description">
+								Close full screen editor
+							</div>
+
+							<div className="key">Left</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		)
+	}
+
 	render() {
 		return (
 			<div className="app-container">
@@ -54,6 +405,18 @@ class App extends Component {
 					</div>
 
 					<div className="light-header">
+						<Popover
+	            popoverClassName="help"
+	            portalClassName="portal"
+	            enforceFocus={false}
+	        	>
+							<Button
+								minimal={true}
+								icon="help"
+								text="Keyboard shortcuts"
+							/>
+              {this.getPopoverContents()}
+	          </Popover>
 					</div>
 				</div>
 
@@ -73,6 +436,7 @@ class App extends Component {
 							</div>
 
 							<div className="sidebar-header-right">
+								<div className="key">Shift+1</div>
 								<Button
 									minimal={true}
 									icon="cross"
@@ -89,36 +453,6 @@ class App extends Component {
 							</div>
 
 							<ul className="dummy-list">
-								<li className="single-dummy-item">
-									<div className="dummy-icon"/>
-									<div className="dummy-bar dummy-bar-3"/>
-								</li>
-
-								<li className="single-dummy-item">
-									<div className="dummy-icon"/>
-									<div className="dummy-bar dummy-bar-2"/>
-								</li>
-
-								<li className="single-dummy-item">
-									<div className="dummy-icon"/>
-									<div className="dummy-bar dummy-bar-4"/>
-								</li>
-
-								<li className="single-dummy-item">
-									<div className="dummy-icon"/>
-									<div className="dummy-bar dummy-bar-1"/>
-								</li>
-
-								<li className="single-dummy-item">
-									<div className="dummy-icon"/>
-									<div className="dummy-bar dummy-bar-3"/>
-								</li>
-
-								<li className="single-dummy-item">
-									<div className="dummy-icon"/>
-									<div className="dummy-bar dummy-bar-3"/>
-								</li>
-
 								<li className="single-dummy-item">
 									<div className="dummy-icon"/>
 									<div className="dummy-bar dummy-bar-2"/>
@@ -230,6 +564,8 @@ class App extends Component {
 									}, "main-area-section-global-code")
 								}
 							>
+								<div className="key">Shift+2</div>
+
 								<Button
 									minimal={true}
 									icon="cross"
@@ -255,20 +591,36 @@ class App extends Component {
 
 											<div className="full-screen-header">
 
-												<div className="full-screen-title">
-													<Icon icon="code" />
-													<span className="section-title">TRANSFORMATION EDITOR</span>
+												<div className="full-screen-header-left">
+													<Button
+														minimal={true}
+														icon="arrow-left"
+														text="Back"
+														className="button-back"
+														onClick={() => { this.setState({
+															fullScreen: false
+														})}}
+													/>
+													<div className="full-screen-title">
+														<Icon icon="code" />
+														<span className="section-title">TRANSFORMATION EDITOR</span>
+													</div>
 												</div>
 
+												<div className="close-button-container">
 
-												<Button
-													minimal={true}
-													rightIcon="cross"
-													text={`Back to ${this.props.pathsVisible ? "Path 1" : "Graph"}`}
-													onClick={() => { this.setState({
-														fullScreen: false
-													})}}
-												/>
+													<div className="key">ESC</div>
+
+													<Button
+														minimal={true}
+														rightIcon="cross"
+														onClick={() => { this.setState({
+															fullScreen: false
+														})}}
+													/>
+
+												</div>
+
 
 											</div>
 
@@ -341,30 +693,11 @@ class App extends Component {
 												</div>
 
 												<ul className="dummy-list">
-													<li className="single-dummy-item single-dummy-item-active">
-														<div className="dummy-icon"/>
-														<div className="dummy-bar dummy-bar-3"/>
-													</li>
 
-													<li className="single-dummy-item">
-														<div className="dummy-icon"/>
-														<div className="dummy-bar dummy-bar-2"/>
-													</li>
-
-													<li className="single-dummy-item">
-														<div className="dummy-icon"/>
-														<div className="dummy-bar dummy-bar-4"/>
-													</li>
-
-													<li className="single-dummy-item">
-														<div className="dummy-icon"/>
-														<div className="dummy-bar dummy-bar-1"/>
-													</li>
-
-													<li className="single-dummy-item">
-														<div className="dummy-icon"/>
-														<div className="dummy-bar dummy-bar-3"/>
-													</li>
+													{this.state.paths.map((path,i) => {
+															return this.renderPath(path, i)
+														})
+													}
 												</ul>
 
 											</div>
@@ -372,78 +705,13 @@ class App extends Component {
 
 										<div className="paths-right-section">
 											<div className="path-container">
-												<div className="path-title">Path 1</div>
+												<div className="path-title">{this.state.paths[this.state.activePath].name}</div>
 												<div className="path-description">
 													Path description here...
 												</div>
 
-												<div className="single-path-step">
-													<Button
-														minimal={true}
-														icon="fullscreen"
-														className="fullscreen"
-														onClick={() => { this.setState({
-																fullScreen: true
-															})}
-														}
-													/>
-												</div>
+												{this.renderPathSteps(this.state.paths[this.state.activePath])}
 
-												<div className="path-arrow"><Icon icon="arrow-down" /></div>
-
-												<div className="single-path-step">
-													<Button
-														minimal={true}
-														icon="fullscreen"
-														className="fullscreen"
-														onClick={() => { this.setState({
-																fullScreen: true
-															})}
-														}
-													/>
-												</div>
-
-												<div className="path-arrow"><Icon icon="arrow-down" /></div>
-
-												<div className="single-path-step">
-													<Button
-														minimal={true}
-														icon="fullscreen"
-														className="fullscreen"
-														onClick={() => { this.setState({
-																fullScreen: true
-															})}
-														}
-													/>
-												</div>
-
-												<div className="path-arrow"><Icon icon="arrow-down" /></div>
-
-												<div className="single-path-step">
-													<Button
-														minimal={true}
-														icon="fullscreen"
-														className="fullscreen"
-														onClick={() => { this.setState({
-																fullScreen: true
-															})}
-														}
-													/>
-												</div>
-
-												<div className="path-arrow"><Icon icon="arrow-down" /></div>
-
-												<div className="single-path-step">
-													<Button
-														minimal={true}
-														icon="fullscreen"
-														className="fullscreen"
-														onClick={() => { this.setState({
-																fullScreen: true
-															})}
-														}
-													/>
-												</div>
 											</div>
 										</div>
 									</div>
@@ -522,6 +790,8 @@ class App extends Component {
 							</div>
 
 							<div className="sidebar-header-right">
+								<div className="key">Shift + 3</div>
+
 								<Button
 									minimal={true}
 									icon="cross"
